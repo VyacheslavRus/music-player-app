b24403b852691f72f8e01fee6e54ef08fc7e48f0
nazarsilveroff <nazar91187@gmail.com>
2022-02-01 14:06:43 +0200

    create start position styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
 import Main from './Components/Main/Main';
 import Navigation from './Components/Navigation/Navigation';
+import {AppWrapper} from "./AppStyled";
 
-function App() {
+const App = () => {
     return (
-        <>
+        <AppWrapper className={'container'}>
             <Navigation/>
             <Main/>
-        </>
+        </AppWrapper>
     );
 }
 
-export default App;
+export default App
