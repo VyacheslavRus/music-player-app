c45fb83ab1b6cd64df27125681206b356a1bcac0
VyacheslavRus <78310890+VyacheslavRus@users.noreply.github.com>
2022-02-04 13:03:14 +0300

    add burger

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,31 +1,50 @@
-import {NavLink, useLocation} from "react-router-dom";
-import {mainRoutes} from "../../routes/mainRoutes";
+import { NavLink, useLocation } from "react-router-dom";
+import { mainRoutes } from "../../routes/mainRoutes";
+import cls from "classnames";
+import { useState } from "react";
+import { NavigationStyled } from "./NavigationStyled";
 
 const Navigation = () => {
-    const location = useLocation()
+  const location = useLocation();
+  const [toglBurger, setToglBurger] = useState(false);
+  const hendleToglBurger = () => {
+    setToglBurger(!toglBurger);
+  };
 
-    return (
-        <>
-            <button>Btn</button>
-            <ul>
-                {mainRoutes.map(({path,name}) => (
-                    <li key={path}>
-                        <NavLink
-                            to={{
-                                pathname:path,
-                                state: {from: location.pathname},
-                            }}
-                            // exact
-                            // className=""
-                            // activeClassName=""
-                        >
-                            {name}
-                        </NavLink>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  return (
+    <NavigationStyled>
+      <button
+        className={cls(toglBurger ? "burgerCloseBtn" : "burgerOpenBtn")}
+        type="button"
+        onClick={hendleToglBurger}
+      >
+        Btn
+      </button>
+      <div
+        className={cls(
+          toglBurger ? "burgerModalWrapperOpen" : "burgerModalWrapperClose"
+        )}
+      >
+        <ul className="burgerList">
+          {mainRoutes.map(({ path, name }) => (
+            <li key={path} className="navItem">
+              <NavLink
+                to={{
+                  pathname: path,
+                  state: { from: location.pathname },
+                }}
+                // exact
+                className="navItemLink"
+                activeclassname="activ"
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </NavigationStyled>
+  );
 };
 
 export default Navigation;
